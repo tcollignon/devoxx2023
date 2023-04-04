50aedc50c6131f5cdee37aeb5d45a7aac239e1e5
Thomas COLLIGNON <t-collignon@efluid.fr>
2023-04-04 21:35:20 +0200

    [fix] Column 2000

--- src/main/java/org/tcollignon/user/object/User.java
+++ src/main/java/org/tcollignon/user/object/User.java
@@ -6,10 +6,7 @@ import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -30,6 +27,7 @@ public class User extends PanacheEntity {
     public String nickname;
     public String name;
     public String firstName;
+    @Column(length = 2000)
     public String desc;
 
     @Username
