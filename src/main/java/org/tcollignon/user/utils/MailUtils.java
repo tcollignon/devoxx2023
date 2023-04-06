d6344a045490adccc44432f037bc89da91287501
Thomas Collignon <t-collignon@efluid.fr>
2023-04-06 22:09:56 +0200

    [fix] No password mandatory in update

--- src/main/java/org/tcollignon/user/utils/MailUtils.java
+++ src/main/java/org/tcollignon/user/utils/MailUtils.java
@@ -5,10 +5,8 @@ public class MailUtils {
     public static String getReinitPasswordMailHtml(String link, String logoPath) {
         String html = "<html>" +
             "<body>\n" +
-            "                        <h2 style=\"margin-bottom:0!important;font-weight:500!important\">\n" +
-            "                          Votre demande de changement de mot de passe\n" +
-            "                        </h2>\n" +
-            "                      Vous avez demand&eacute; &agrave; r&eacute;initialiser votre mot de passe. Cliquez sur le lien ci-dessous pour en choisir un nouveau.\n" +
+            "                        <h2>Votre demande de changement de mot de passe</h2>\n" +
+            "                      Cliquez sur le lien ci-dessous pour en choisir un nouveau.\n" +
             "                        <a href=\"" + link + "\" target=\"_blank\">Changer mon mot de passe</a>\n" +
             "                        <br>The Devoxx Team\n" +
             "                    </body>" +
@@ -18,7 +16,7 @@ public class MailUtils {
 
     public static String getRegisterMailHtml(String nickname) {
         String html = "<html>" +
-            "<body> Bienvenue &agrave; Devoxx " + nickname +
+            "<body> Bienvenue dans cette application Devoxx " + nickname +
             "</body>" +
             "</html>";
         return html;
